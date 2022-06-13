96f7f422bd0cfd1bde3815c0163a8620298037a4
Chase Edwards <chase.w.edwards9@gmail.com>
2022-06-12 20:00:04 -0400

    Add style, structure to the tutorial dialog

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -11,18 +11,25 @@ import { DomSanitizer } from "@angular/platform-browser";
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer, private dialog: MatDialog) {
+  constructor(
+    private matIconRegistry: MatIconRegistry, 
+    private domSanitizer: DomSanitizer, 
+    private dialog: MatDialog
+  ) {
     this.matIconRegistry.addSvgIcon(
       `menu`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl("./assets/images/menu.svg")
+      this.domSanitizer
+        .bypassSecurityTrustResourceUrl("./assets/images/menu.svg")
     );
     this.matIconRegistry.addSvgIcon(
       `questionMark`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl("./assets/images/question-mark.svg")
+      this.domSanitizer
+        .bypassSecurityTrustResourceUrl("./assets/images/question-mark.svg")
     );
     this.matIconRegistry.addSvgIcon(
       `settings`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl("./assets/images/settings.svg")
+      this.domSanitizer
+        .bypassSecurityTrustResourceUrl("./assets/images/settings.svg")
     );
   }
 
@@ -30,7 +37,12 @@ export class NavbarComponent implements OnInit {
   }
 
   openTutorialDialog() {
-    const dialogRef = this.dialog.open(TutorialDialogComponent);
+    const dialogRef = this.dialog.open(TutorialDialogComponent, {
+      position: {
+        top: "50px"
+      },
+      maxHeight: "calc(100vh - 75px)"
+    });
 
     dialogRef.afterClosed().subscribe(result => {
       console.log(`Dialog result: ${result}`);

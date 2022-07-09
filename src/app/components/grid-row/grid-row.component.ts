25158343af05439975ea0942ff8171a62fcd9107
Chase Edwards <chase.w.edwards9@gmail.com>
2022-07-09 11:55:07 -0400

    Add props to templates for tracking each ind. row/tile and subscribe to guessedWords observable to populate in each grid row/tile.

--- src/app/components/grid-row/grid-row.component.ts
+++ src/app/components/grid-row/grid-row.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , Input} from '@angular/core';
+import { filter, map, tap } from 'rxjs/operators';
+import { MainService } from '../main/main.service';
 
 @Component({
   selector: 'grid-row',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./grid-row.component.scss']
 })
 export class GridRowComponent implements OnInit {
+  @Input() rowNum!: number;
 
-  constructor() { }
+  guessedWords$ = this.mainService.guessedWords$;
+
+  rowWord!: string;
+
+  constructor(private mainService: MainService) { }
 
   ngOnInit(): void {
+    // this.guessedWords$.subscribe(v => console.log(v))
+
+    this.guessedWords$.pipe(
+      map((words) => {
+        return words.filter((word, idx) => {
+          return idx === this.rowNum
+        })
+      })
+    ).subscribe(value => {
+      console.log(value)
+      this.rowWord = value[0]
+    })
+
+    
+  
   }
+
+  
 
 }

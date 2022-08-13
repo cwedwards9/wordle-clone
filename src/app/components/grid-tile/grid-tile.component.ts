eb37e2314efa2311aef96e7474359b8a4d04e881
Chase Edwards <chase.w.edwards9@gmail.com>
2022-08-13 15:53:36 -0400

    Add logic for styling the letter tiles in each guessed word based on its position.

--- src/app/components/grid-tile/grid-tile.component.ts
+++ src/app/components/grid-tile/grid-tile.component.ts
@@ -13,10 +13,12 @@ export class GridTileComponent implements OnInit {
   @Input() rowNum!: number;
 
   tileLetter!: string;
+  letterStatus!: string;
 
   guessedWordsList$ = this.mainService.guessedWordsList$;
   guessedWord$ = this.mainService.guessedWord$;
   currentRow$ = this.mainService.currentRow$;
+  currentWord$ = this.mainService.currentWord$
 
   constructor(private mainService: MainService) { }
 
@@ -31,11 +33,21 @@ export class GridTileComponent implements OnInit {
       map(word => {
         return word ? word : "";
       })
-    ).subscribe(word => {
-      this.tileLetter = word[this.tileNum]
+    ).subscribe(guessedWord => {
+      this.tileLetter = guessedWord[this.tileNum]
     })
 
-    // this.currentRow$.pipe(combineLatest)
+    // compare letter to the current, real, word's letter in this index
+    this.currentWord$.subscribe(currentWord => {
+      if(this.tileLetter === currentWord[this.tileNum]) 
+        this.letterStatus = "correct";
+      else if(currentWord.includes(this.tileLetter)) 
+        this.letterStatus = "close";
+      else if(!currentWord.includes(this.tileLetter) && !!this.tileLetter) 
+        this.letterStatus = "incorrect";
+      else this.letterStatus = "";
+    })
+
 
     combineLatest([this.currentRow$, this.guessedWord$]).pipe(map(([currentRow, guessedWord]) => {
       if(currentRow === this.rowNum) {

e01bb1cb731a4a31b08269e4dcedd80d8e15323a
Chase Edwards <chase.w.edwards9@gmail.com>
2022-10-30 18:35:08 -0400

    Update navbar icons as button icons and import and update buttons to use mat button module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { TutorialDialogComponent } from './components/tutorial-dialog/tutorial-d
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { HttpClientModule } from "@angular/common/http";
 import { MatDialogModule } from '@angular/material/dialog';
 import {MatDividerModule} from '@angular/material/divider';
@@ -39,6 +40,7 @@ import { StatisticsDialogComponent } from './components/statistics-dialog/statis
     BrowserModule,
     BrowserAnimationsModule,
     MatIconModule,
+    MatButtonModule,
     HttpClientModule,
     MatDialogModule,
     MatDividerModule
